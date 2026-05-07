18ff04a3e46ce9bc0ca20a07e16201fea35e9d52
Norah-alkubra1 <445052179@std.psau.edu.sa>
2026-05-07 04:57:19 -0700

    adding lock to protccte shared variable contextSwitchCount

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -51,7 +51,15 @@ public static final Semaphore cpuSemaphore = new Semaphore(1);
     public static void incrementContextSwitch() {
         // TODO: Protect this critical section with a lock
         // RACE CONDITION: Multiple threads might read and write simultaneously!
-        contextSwitchCount++;
+    lock.lock();
+
+try {
+    contextSwitchCount++;
+} finally {
+    lock.unlock();
+}
+
+
     }
     
     // Method to increment completed process counter
