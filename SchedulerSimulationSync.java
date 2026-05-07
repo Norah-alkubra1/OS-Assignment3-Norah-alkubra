d2c729769357eed3b65f2d986a08abc0fe3be8df
Norah-alkubra1 <445052179@std.psau.edu.sa>
2026-05-07 05:09:57 -0700

    add semaphore synchronization

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -126,6 +126,16 @@ class Process implements Runnable {
         // TODO #3: Acquire CPU semaphore before executing
         // This ensures only allowed number of processes run simultaneously
         
+        // Acquire CPU semaphore before execution
+try {
+    SharedResources.cpuSemaphore.acquire();
+} catch (InterruptedException e) {
+    e.printStackTrace();
+}
+
+
+
+
         try {
             if (startTime == -1) {
                 startTime = System.currentTimeMillis();
@@ -188,6 +198,10 @@ class Process implements Runnable {
         } finally {
             // TODO #4: Release CPU semaphore here
             // Always release in finally block to prevent deadlocks!
+
+// Release CPU semaphore after execution
+SharedResources.cpuSemaphore.release();
+
         }
     }
     
